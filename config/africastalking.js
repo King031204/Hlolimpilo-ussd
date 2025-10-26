4caa3e71f4670cd16b5a3665f0abecfb1714457a
King031204 <161839488+King031204@users.noreply.github.com>
2025-10-26 21:04:11 +0200

    Create africastalking.js

---
+++ config/africastalking.js
@@ -0,0 +1,8 @@
+const Africastalking = require("africastalking");
+
+const africastalking = Africastalking({
+  apiKey: process.env.AT_API_KEY,
+  username: process.env.AT_USERNAME,
+});
+
+module.exports = africastalking;
